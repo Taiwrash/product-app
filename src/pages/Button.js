2b5d1665829cc278090b211bfdf1498e1388f1da
Taiwrash <rasheedrtm1@gmail.com>
2020-11-29 20:40:36 +0100

    developm commit
    Signed-off-by: Taiwrash <rasheedrtm1@gmail.com>

---
+++ src/pages/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <>
+      <div className="btn-secondary btn-parent">
+        <button>Edit</button>
+        <button>Delete</button>
+        <button>Like</button>
+      </div>
+    </>
+  );
+};
+
+export default Button;
